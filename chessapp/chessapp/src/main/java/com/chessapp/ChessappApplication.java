60bf472e99bf379e54b315d69bd891adbce73b95
eeshunarayan16 <eeshunarayan16@gmail.com>
2024-11-01 15:02:47 +0530

    Add files via upload

---
+++ chessapp/chessapp/src/main/java/com/chessapp/ChessappApplication.java
@@ -0,0 +1,14 @@
+package com.chessapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessappApplication {
+
+	public static void main(String[] args) {
+	
+		SpringApplication.run(ChessappApplication.class, args);
+	}
+
+}
